15eba4fa3c1a9695b90b50ceb1085b488d182290
camilocona <95385086+camilocona@users.noreply.github.com>
2023-09-20 00:11:06 -0500

    Update controlunit.sv
    Actualización 20/09/2023

--- controlunit.sv
+++ controlunit.sv
@@ -8,13 +8,45 @@ module controlunit (clk, reset, loaddata, inputdata_ready);
 
 	// Internal signals for state machine
 	// WRITE HERE YOUR CODE
+	
+	//           Carga de datos / Multiplicacion
+	typedef enum logic [2:0] {s0, s1} State;
+	State currentState, nextState;
+
 
 	// Process (Sequential): update currentState
-	// WRITE HERE YOUR CODE
-	
+	always_ff @(posedge reset) 
+		if (reset)
+			currentState <= s0;
+		else 
+			currentState <= nextState;
+			
 	// Process (Combinational): update nextState
-	// WRITE HERE YOUR CODE
+	always_comb begin	
+			case (currentState)
+				s0:
+				//DUDA
+					if(inputdata_ready==0) begin
+						loaddata==1;
+						nextState=s1;
+					end
+					else
+						nextState= s0;
+				s1:
+				//DUDA
+					if(inputdata_ready==1) begin
+						loaddata=0;
+						nextState=s0;
+					end
+					else 
+						nextState= s1;
+					
+				default:		
+					nextState = s0;
+			endcase
+		end
 
+		//Como definir las salidas??a
 	// Process (Combinational): update outputs 
 	// WRITE HERE YOUR CODE
 endmodule
