b8573f5a7bbbfcd94982bbee67c4a1fbfff9e32d
Valentina-Q-A <62760903+Valentina-Q-A@users.noreply.github.com>
2023-09-28 10:10:31 -0500

    Update controlunit.sv

--- controlunit.sv
+++ controlunit.sv
@@ -5,9 +5,6 @@ module controlunit (clk, reset, loaddata, inputdata_ready);
 	input logic  clk, reset;
 	output logic  loaddata;
 	input logic inputdata_ready;
-
-	// Internal signals for state machine
-	// WRITE HERE YOUR CODE
 	
 	//           Carga de datos / Multiplicacion
 	typedef enum logic [2:0] {s0, s1} State;
@@ -25,23 +22,24 @@ module controlunit (clk, reset, loaddata, inputdata_ready);
 	always_comb begin	
 			case (currentState)
 				s0:
-				//DUDA
 					if(inputdata_ready==0) begin
-						loaddata=1;
 						nextState=s0;
+						loaddata=1;
 					end
 					else begin
 						nextState= s1;
-						loaddata=0; end
-				s1:
-				//DUDA
-					if(inputdata_ready==1) begin
 						loaddata=0;
+						end
+				s1:
+					if(inputdata_ready==1) begin
 						nextState=s1;
+						loaddata=0;
 					end
 					else begin
 						nextState= s0;
-						loaddata=1; end
+						loaddata=1;
+						
+						 end
 					
 				default:		
 					nextState = s0; 
@@ -49,14 +47,4 @@ module controlunit (clk, reset, loaddata, inputdata_ready);
 			endcase
 		end
 
-		//Como definir las salidas??a
-	// Process (Combinational): update outputs 
-	// WRITE HERE YOUR CODE
-endmodule
-
-// ************************** 
-// Testbench for Control Unit
-// ************************** 
-module tb_controlunit ();
-	// WRITE HERE YOUR CODE
 endmodule
