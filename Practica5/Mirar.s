fcdcf642ef93ba6e5e7ba3f95954a0ffb8146a3d
Valentina-Q-A <62760903+Valentina-Q-A@users.noreply.github.com>
2023-10-18 18:35:21 -0500

    Update Mirar.s

--- Practica5/Mirar.s
+++ Practica5/Mirar.s
@@ -6,82 +6,75 @@ _start:
 
 
 LDR R2, =Data //Lista de datos sin arreglar s
+LDR R5,=SortedData
 MOV R0, #0 // i
 LDR R1, =N
 LDR R1, [R1]
 SUB R1, #1//j=N-1
-//------------------------------DUDA CAMI-------------------
+PUSH {R0}
+BL copy_data
+POP {R0}
+LDR R3, =Data2 //Lista de salida arreglada c
+BL mergeSort
+B stop
+//-------------------------------------------------
 
-PUSH {LR,R0}
-copy_data:
+copy_data: 
     CMP R0, R1
 	BGE DONE
-	LDR R5, [R2, R0, LSL #2]	
-	STR R5, [R2, R0, LSL #2]	
+	LDR R3, [R2, R0, LSL #2]	
+	STR R3, [R5, R0, LSL #2]	
 	ADD R0,R0, #1
 	B copy_data
 DONE:
-	POP {LR,R0}
 	MOV PC, LR
 	
 
-
-
-//------------------------------------------------------------------------
-
-//------------------------------------------------------------------------
-
-BL mergeSort
-B stop
-
+//--------------------------------------------------
 mergeSort:  //(s,i,j)
-	PUSH {LR} 
-	LDR R3, =Data2 //Lista de salida arreglada c
+	PUSH {LR,R4,R8} 
+	
 If1:
 	CMP R0,R1
 	BNE CONTINUE  //Cuando i!=j
-	POP {LR}  //VOLVEMOS A CARGAR ANTES DEL RETORNAR
+	POP {LR,R4,R8}  //VOLVEMOS A CARGAR ANTES DEL RETORNAR
 	MOV PC, LR
 	
 
 CONTINUE:
 	ADD R4,R0,R1 //M=  i+j FALTA EL /2
 	LSR R4, R4, #1 //M=(i+j)/2   
-	//LLAMAR POR PRIMERA VEZ MERGESORT   ------DUDA-------
-	PUSH {R0,R1,R2,R4} 
+	//LLAMAR POR PRIMERA VEZ MERGESORT   
+	PUSH {R0,R1,R4} 
 	MOV R1,R4 //J<-M
 	BL mergeSort   //MergeSort(s,i,m) 
-	POP {R0,R1,R2,R4}
-	
+	POP {R0,R1,R4}
+
 	//------------
-	PUSH {R0,R1,R2,R4}
+	PUSH {R0,R1,R4}
 	ADD R4,R4,#1 //M = M+1
 	MOV R0,R4 // I <- M+1
 	BL mergeSort   //MergeSort(s,m+1,j)
-	POP {R0,R1,R2,R4}
+	POP {R0,R1,R4}
 	
 
 
-	
-//DUDA DE COMO ENTRARLE I,J A FUSE
-	PUSH {R0,R1,R3}  //ENTRADA FUSE(C,I,J)
+	  //ENTRADA FUSE(C,I,J,M)
 	BL fuse
-	POP {R0,R1,R3}
-
-for1:
-	MOV R8,#0 //K que recorre el fuse
-
 	MOV R8, R0  //K <- I
-	//LDR
+	
+for1:
+
 	CMP R8, R1
-	LDR R8,[R8]
-	
-	LDR R12, [R3, R8, LSL #2] //SE GUARDA LA PRIMERA DE LA LISTA DE S    
-	STR R12, [R5, R8, LSL #2] //SE GUARDA LA PRIMERA DE LA LISTA DE S EN C
+	BGT END
+	LDR R12,[R3,R8, LSL #2] //Ck
+	STR R12,[R5,R8, LSL #2] //Sk<-Ck
 	ADD R8, R8, #1 //se recorre la lista K++
-	BLE for1
 	
-	POP {LR}  //VOLVEMOS A CARGAR ANTES DEL RETORNAR
+	B for1
+	
+	END:
+	POP {LR,R4,R8}  //VOLVEMOS A CARGAR ANTES DEL RETORNAR
 	MOV PC, LR
 
 
@@ -89,8 +82,9 @@ for1:
 
 
 fuse:
+	PUSH {R6-R11}
 	MOV R9, R0 //P <-I
-	MOV R10,R4 //Q <- M+1
+	ADD R10, R4, #1 //Q <- M+1
 	MOV R11, R0 //R <- I
 
 WHILE1:
@@ -100,38 +94,47 @@ WHILE1:
 	CMP R10,R1
 	BGT WHILE2 //VA A WHILE2 SI Q>J
 	B IFW1
-	ADD R11,R11,#1  //R++
 
 IFW1:
-	MOV R6,#0 // Sp
-	MOV R7,#0 // Sq
-	MOV R8,#0 //SE REUTILIZA PARA Cr
-	LDR R6,[R2,R9, LSL #2] //Sp
-	LDR R7,[R2,R10, LSL #2] //Sq
-	LDR R8,[R3,R11, LSL #2] //Cr
+
+	LDR R6,[R5,R9, LSL #2] //Sp
+	LDR R7,[R5,R10, LSL #2] //Sq
 	CMP R6,R7
 	BGE ELSEW1 //SI R6>=R7 SE VA A ELSEW1
-	STR R6,[R8]   //Cr <- SP
+	STR R6,[R3,R11, LSL #2] //Cr<-SP
 	ADD R9,R9,#1 //P++
+	B END_IF
 
 ELSEW1:
-	MOV R8,R7  //Cr <- Sq
+	STR R7,[R3,R11, LSL #2] //Cr<-SQ
 	ADD R10,R10,#1 //Q++
+	
+END_IF:
+	ADD R11,R11,#1  //R++
+	B WHILE1
 
 WHILE2:
-	CMP R9,R4
+	
+	CMP R9,R4 
 	BGT WHILE3 //VA A WHILE2 SI P>M
-	MOV R8,R6   //Cr <- Sp
+	LDR R6,[R5,R9, LSL #2] //Sp
+	STR R6,[R3,R11, LSL #2] //Cr<-SP
 	ADD R9,R9,#1 //P++
 	ADD R11,R11,#1  //R++
-
+	B WHILE2
+	
 WHILE3:
+	
 	CMP R10,R1
-	BGT WHILE2 //VA A WHILE2 SI Q>J
-	MOV R8,R7  //Cr <- Sq
+	BGT DONE_FUSE //VA A WHILE2 SI Q>J
+	LDR R7,[R5,R10, LSL #2] //Sq
+	STR R7,[R3,R11, LSL #2] //Cr<-SQ
 	ADD R10,R10,#1 //Q++	
 	ADD R11,R11,#1  //R++
+	B WHILE3
 	
+DONE_FUSE:
+	POP {R6-R11}
 	MOV PC,LR
 	
 
