5c13ac1cb55e11848cd69a08a0710c91a80d7d0e
camilocona <95385086+camilocona@users.noreply.github.com>
2023-10-29 21:10:05 -0500

    Update controller.sv

--- Practica6/controller.sv
+++ Practica6/controller.sv
@@ -11,21 +11,20 @@ module controller(input logic clk, reset,
 						output logic [1:0] ALUControl,
 						output logic MemWrite, MemtoReg,
 						output logic PCSrc
-						output logic Shift //LSL O LSR
-						
-						output logic ASR
-						output logic ROR
+						output logic Shift //LSL, LSR, ROR Y ASR
 						output logic MOV
-						);
+						output logic B);
+						
 	logic [1:0] FlagW;
 	logic PCS, RegW, MemW;
+	logic NoWrite; // LSL, LSR, ASR, ROR
 
 	decoder dec(Instr[27:26], Instr[25:20], Instr[15:12],
 					FlagW, PCS, RegW, MemW,
-					MemtoReg, ALUSrc, ImmSrc, RegSrc, ALUControl,Shitf,ASR,ROR,MOV);
+					MemtoReg, ALUSrc, ImmSrc, RegSrc, ALUControl,NoWrite, Shift);
 
 	condlogic cl(clk, reset, Instr[31:28], ALUFlags,
 					FlagW, PCS, RegW, MemW,
-					PCSrc, RegWrite, MemWrite,Shift, ASR,ROR,MOV);
+					PCSrc, RegWrite, MemWrite, NoWrite);
 
 endmodule
