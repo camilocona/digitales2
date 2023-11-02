a2493d085da6c38c539f52919e184df2d10cf469
camilocona <95385086+camilocona@users.noreply.github.com>
2023-11-01 19:23:05 -0500

    Add files via upload

---
+++ Practica6/shift.sv
@@ -0,0 +1,20 @@
+
+module shift (
+	input logic [31:0] entrada,
+	input logic [6:0] instruccion,
+	output logic [31:0] salida
+);
+
+	logic [1:0] control;
+
+	assign control = instruccion [1:0];
+
+	always_comb begin
+		case (control)
+		2'b00: salida = entrada << instruccion [6:2];							 //LSL
+		2'b01: salida = entrada >> instruccion [6:2];							 //LSR
+		2'b10: salida = entrada >>> instruccion [6:2];							 //ASR
+		2'b11: salida = (entrada >> instruccion [6:2]) | (entrada << 32);	 //ROR
+		endcase
+	end
+endmodule
