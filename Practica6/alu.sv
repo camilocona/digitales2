02812a465466dd9650dc42491ff587f8ed50c251
camilocona <95385086+camilocona@users.noreply.github.com>
2023-11-01 19:18:57 -0500

    Update alu.sv

--- Practica6/alu.sv
+++ Practica6/alu.sv
@@ -2,7 +2,7 @@
  * This module is the ALU of the Datapath Unit
  */ 
 module alu #(parameter N = 4) (A, B, ALUControl, Result, ALUFlags);
-	input logic  [1:0] ALUControl;
+	input logic  [2:0] ALUControl;
 	input logic  [N-1:0] A, B;
 	output logic [N-1:0] Result;
 	output logic [3:0] ALUFlags;
@@ -12,11 +12,14 @@ module alu #(parameter N = 4) (A, B, ALUControl, Result, ALUFlags);
 	always_comb begin
 		Cout = 1'b0;
 		case (ALUControl)
-			2'b11: begin
+			3'b011: begin
 				Result = A | B;
 			end
-			2'b10: begin
+			3'b010: begin
 				Result = A & B;
+			end
+			3'b101: begin
+				Result= B;
 			end
 			default: begin
 				{Cout, Result} = {1'b0, A} + {1'b0, (ALUControl[0] == 1'b0 ? B : ~B)} + ALUControl[0];
@@ -33,4 +36,3 @@ module alu #(parameter N = 4) (A, B, ALUControl, Result, ALUFlags);
 	// Overflow
 	assign ALUFlags[0] = (~(ALUControl[0] ^ A[N-1] ^ B[N-1])) & (A[N-1] ^ Result[N-1]) & (~ALUControl[1]);
 endmodule
-
