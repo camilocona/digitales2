660aa780c99f3304f1159ef1485e02395b56f347
Valentina-Q-A <62760903+Valentina-Q-A@users.noreply.github.com>
2023-11-06 11:45:58 -0500

    Update imem.sv

--- Practica6/imem.sv
+++ Practica6/imem.sv
@@ -1,7 +1,7 @@
 /*
  * This module is the Instruction Memory of the ARM single-cycle processor
  */ 
-module imem(input logic [31:0] a, output logic [31:0] rd);
+/*module imem(input logic [31:0] a, output logic [31:0] rd);
 	// Internal array for the memory (Only 64 32-words)
 	logic [31:0] RAM[63:0];
 
@@ -9,7 +9,7 @@ module imem(input logic [31:0] a, output logic [31:0] rd);
 	// Be careful to have a program longer than the memory available
 	initial
 		// Uncomment only the following line to load the code from the book (testbench)
-		$readmemh("C:/Digitales2/ARM-SingleCycle/imem_tb_from_book.dat",RAM);
+		//$readmemh("C:/Digitales2/ARM-SingleCycle/imem_tb_from_book.dat",RAM);
 		
 		// Uncomment only the following line to load the code given to check peripherals
 		// $readmemh("imem_to_test_peripherals.dat",RAM);
@@ -18,4 +18,4 @@ module imem(input logic [31:0] a, output logic [31:0] rd);
 		// $readmemh("imem_made_by_students.dat",RAM);
 
 	assign rd = RAM[a[31:2]]; // word aligned
-endmodule
+endmodule*/
